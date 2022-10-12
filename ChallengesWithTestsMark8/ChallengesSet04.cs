88a983898ce8519bf378cecf3f5dee3c3baeda16
cmreavis97 <cmreavis97@gmail.com>
2022-10-12 15:24:38 -0400

    set 4 complete

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +10,131 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var sum = 0;
+            foreach(var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num;
+                }
+                if (num %2 != 0)
+                {
+                    sum -= num;
+                }
+            }return sum;
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            List<int> stringLengths = new List<int>
+            {
+                str1.Length,
+                str2.Length,
+                str3.Length,
+                str4.Length
+            
+            };
+            return stringLengths.Min();
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            List<int> numbers = new List<int>
+            {
+                number1,
+                number2,
+                number3,
+                number4
+            };
+            return numbers.Min();
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return (sideLength1 + sideLength2 > sideLength3 && 
+                    sideLength1 + sideLength3 > sideLength2 && 
+                    sideLength2 + sideLength3 > sideLength1);
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool isNumber = double.TryParse(input, out double number);
+            return isNumber;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var arrayLength = objs.Length;
+            var majorityOfElements = (arrayLength / 2) + 1;
+
+            var numOfNullElements = 0;
+
+            foreach (var element in objs)
+            {
+                if (element == null)
+                {
+                    numOfNullElements++;
+                }
+            }
+
+            if(numOfNullElements >= majorityOfElements)
+            {
+                return true;
+            }
+            else return false;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+            
+            //List<double> evens = new List<double>();   // <-- Will try this way later
+            //for(int i = 0; i < numbers.Length; i++)
+            //{
+            //    if (numbers[i] % 2 == 0)
+            //    {
+            //        evens.Add(numbers[i]);  
+            //    }
+            //}return evens.Average();
+            var sum = 0.0;
+            var evensCount = 0.0;
+
+            foreach (var num in numbers)
+            {
+                if (num % 2 == 0)
+                {
+                    sum += num; //sum = sum + num
+                    evensCount++;
+                }
+            }
+            if (evensCount > 0)
+            {
+                return sum / evensCount;
+            }
+            else return 0;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            var factorial = 1;
+
+            if (number < 0)
+            {
+                throw new ArgumentOutOfRangeException(); //I should have read the tests page sooner
+            }
+            for (int i = number; i > 0; i--)
+            {
+                factorial *= i;
+            }
+            return factorial;
         }
     }
 }
