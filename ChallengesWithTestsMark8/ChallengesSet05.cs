094dc31d90d8a0d9a1b77184eade2dcae8444350
cmreavis97 <cmreavis97@gmail.com>
2022-11-04 10:34:00 -0400

    finished set 6

--- ChallengesWithTestsMark8/ChallengesSet05.cs
+++ ChallengesWithTestsMark8/ChallengesSet05.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,37 +9,108 @@ namespace ChallengesWithTestsMark8
     {
         public int GetNextNumberDivisibleByN(int startNumber, int n)
         {
-            throw new NotImplementedException();
+            return ((startNumber / n) + 1) * n;
         }
 
         public void ChangeNamesOfBusinessesWithNoRevenueTo_CLOSED(Business[] businesses)
         {
-            throw new NotImplementedException();
+            //for businesses with Business.TotalRevenue = 0, Name = CLOSED
+            foreach (Business business in businesses)
+            {
+                if (business.TotalRevenue == 0)
+                {
+                    business.Name = "CLOSED";
+                }
+            }
         }
 
         public bool IsAscendingOrder(int[] numbers)
         {
-            throw new NotImplementedException();
+            if(numbers == null || numbers.Length == 0)
+            {
+                return false;
+            }
+
+            for(int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] < numbers[i - 1])
+                {
+                    return false;
+                }
+            }return true;
         }
 
         public int SumElementsThatFollowAnEven(int[] numbers)
         {
-            throw new NotImplementedException();
+            var sum = 0;
+
+            if (numbers == null || numbers.Length == 0)
+            {
+                return 0;
+            }
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                //number to be tested as even = [i-1], numbers that follow the even = [i]
+                if (numbers[i-1] % 2 == 0)
+                {
+                    sum += numbers[i];
+                }
+            }return sum;
         }
 
         public string TurnWordsIntoSentence(string[] words)
         {
-            throw new NotImplementedException();
+            string sentence = "";
+            if (words == null || words.Length == 0)
+            {
+                return "";
+            }
+            foreach(string word in words)
+            {
+                if(word.Trim().Length > 0)
+                {
+                    sentence += (word.Trim() + " ");
+                }
+            }
+            if (sentence.Length == 0) 
+            { 
+                return "";
+            }
+            sentence = sentence.Substring(0, sentence.Length - 1);
+            sentence += ".";
+            return sentence;
+                    
         }
 
         public double[] GetEveryFourthElement(List<double> elements)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0)
+            {
+                return new double[0];
+            }
+            List<double> everyFourthElement = new List<double>();
+
+            for(int index = 3; index < elements.Count; index+=4)
+            {
+                everyFourthElement.Add(elements[index]);
+            }
+            return (everyFourthElement.ToArray());
         }
 
         public bool TwoDifferentElementsInArrayCanSumToTargetNumber(int[] nums, int targetNumber)
         {
-            throw new NotImplementedException();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                //double loop to compare two nums
+                for (int x = i + 1; x < nums.Length; x++)
+                {
+                    if (nums[i] + nums[x] == targetNumber)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
         }
     }
 }
--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,121 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            var containsWord = false;
+            if(Equals(words, null) || words.Contains(null))
+            {
+                return false;
+            }
+
+            if (ignoreCase == true)
+            {
+                word.ToLower();
+                List<string> strings = words.Select(x => x.ToLower()).ToList();
+                containsWord = strings.Contains(word);
+            }
+            if(ignoreCase == false)
+            {
+                containsWord = words.Contains(word);
+            }
+            return containsWord;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            bool isPrime = false;
+            if(num >= 0)
+            {
+                if(num % 2 != 0 && num % 3 != 0)
+                {
+                    isPrime = true;
+                }
+            }
+            if(num == 1)
+            {
+                isPrime = false;
+            }
+            if((num == 2) || (num == 3))
+            {
+                isPrime = true;
+            }
+            return isPrime;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            int index = -1;
+
+            for(int i = 0; i < str.Length; i++)
+            {
+                var unique = true;
+
+                for(int x = 0; x < str.Length; x++)
+                {
+                    if (str[i] == str[x] && i != x)
+                    {
+                        unique = false;
+                    }
+                }
+                if(unique == true)
+                {
+                    index = i;
+                }
+            }
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            int count = 0;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                int newCount = 1;
+
+                {
+                    for(int x = i + 1; x < numbers.Length; x++)
+                    {
+                        if (numbers[i] != numbers[x])
+                        {
+                            break;
+                        }
+                        newCount++;
+                    }
+                    if(newCount > count)
+                    {
+                        count = newCount;
+                    }
+                }
+            }
+            return count;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            List<double> element = new List<double>();
+            double[] bonk = new double[0];
+
+            //returns an array just to check for null
+            if(elements == null)
+            {
+                return bonk;
+            }
+
+            for(int i = 0; i < elements.Count; i++)
+            {
+                if (elements[i] % n == 0)
+                {
+                    element.Add(elements[i]);
+                }
+                if(n < 0 || n > elements.Count)
+                {
+                    //clears the list
+                    element.Clear();
+                }
+            }
+            //list to array
+            double[] result = element.ToArray();
+            return result;
         }
     }
 }
